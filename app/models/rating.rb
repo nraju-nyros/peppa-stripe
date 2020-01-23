609aa201aad56b9198fcd4186f80598d60e7f581
nraju-nyros <nraju.nyros@gmail.com>
2020-01-23 12:24:26 +0530

    new

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,6 +3,6 @@ class Rating < ApplicationRecord
 	belongs_to :user
 	
 	
-  validates_presence_of :rating,message:"select rating"
+    validates_presence_of :rating,message:"select rating"
 	validates_presence_of :comments,message:"can't be empty"
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
 	has_and_belongs_to_many :locations, :dependent => :destroy
-  accepts_nested_attributes_for :locations
+    accepts_nested_attributes_for :locations
 
 	has_and_belongs_to_many :dishes, :dependent => :destroy
 	
@@ -12,7 +12,7 @@ class Restaurant < ApplicationRecord
 	validates_associated :locations
 
 	validates_presence_of :name,message:"can't be empty"
-  validates_presence_of :phone, message:"can't be empty", :presence => true,:numericality => true
+    validates_presence_of :phone, message:"can't be empty", :presence => true,:numericality => true
 	validates :email, presence:true, format: { with: /(\A([a-z]*\s*)*\<*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\>*\Z)/i } 
 	
 
